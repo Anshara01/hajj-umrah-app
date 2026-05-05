db3de57ab0cd2678ea72edd04ae939b0ccdd9874
Anshara01 <ansharazahoor9@gmail.com>
2026-05-06 00:38:29 +0500

    Fix web execution blank screen by adding DefaultFirebaseOptions

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'core/utils/app_router.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
